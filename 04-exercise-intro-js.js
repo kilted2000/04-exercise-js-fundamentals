aa625ee7b8af370803b6d6c17fa9c5a37bb24563
kilted2000 <53500493+kilted2000@users.noreply.github.com>
2022-04-30 21:52:12 -0500

    Add files via upload

---
+++ 04-exercise-intro-js.js
@@ -0,0 +1,111 @@
+/* Guess what answers you would get if you ran this in the Javascript Console in Google Chrome. Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line */ 
+
+// 1. Evaluate each of the following expressions
+/* 
+
+1. 5 + "34" 
+534
+2. 5 - "4"
+1
+3. 10 % 5
+0
+4. 5 % 10
+5
+5. "Java" + "Script" 
+JavaScript
+6. " " + " " 
+two combined empty strings
+7. " " + 0 
+0
+8. true + true  
+2
+9. true + false 
+1
+10. false + true 
+1
+11. false - true 
+-1
+12. 3 - 4 
+-1
+13. "Bob" - "bill" 
+NaN
+*/
+
+
+
+
+// 2. Evaluate each of the following comparisons. What is different about these "comparisons" and the previous "expressions?"
+/* 
+
+    1. 5 >= 1 /true
+    2. 0 === 1 /false
+    3. 4 <= 1 /false
+    4. 1 != 1 /false
+    5. "A" > "B" /false
+    6. "B" < "C" /true
+    7. "a" > "A" /true
+    8. "b" < "A" /false
+    9. true === false /false
+    10. true != true /false
+
+*/
+
+// 3. Make the string: "Hi There! It's "sunny" out" by using the + sign.
+/* 
+    Place your answer here
+console.log( "Hi There!" + " " + "It\'s \"sunny\" out");
+Hi There! It's "sunny" out
+*/
+
+// 4. add variable "firstName" and "lastName" - so that they equal your name
+/* 
+    Place your answer here
+let firstName = "Richard";
+let lastName = "Whittington";
+console.log(firstName + " " + lastName); */
+
+
+
+
+
+
+
+// 5. create a variable that holds the answer // of "firstName" + " " + "lastName"
+/* 
+    Place your answer here
+let firstName = "Richard";
+let lastName = "Whittington";
+let name = firstName + " " +lastName;
+console.log(name);
+
+*/
+
+// 6. What is a + b?
+//23
+/* 
+    var a = 34;
+    var b = 21;
+    a = 2;
+*/
+
+// 7. For the previous question, what is 'c' equal to?
+/* 
+    Place your answer here
+    The variable 'c' hasn't been created yet. So it has not value.
+*/
+
+// 8. Make a Calculator! using prompt(), and variables, make a program that does the following:
+
+// a. Prompts the user for first number.
+// b. Stores that first number
+// c. Prompts the user for the second number.
+// d. stores that number and responds with the SUM by using an alert. 
+
+/* 
+    Place your answer here
+let num1 = prompt("Please, input one number.")
+let num2 = prompt("Please, input another number.")
+let sum = parseInt(num1)+parseInt(num2);
+alert(sum);
+
+*/
